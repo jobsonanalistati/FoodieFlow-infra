ddae1d5ba02d282ff6a2c5c6a86b19d463feb996
Guilherme-Endrigo <endrigo.guilherme@hotmail.com>
2024-03-19 00:28:32 -0300

    ajuste eks policy

--- eks.tf
+++ eks.tf
@@ -1,3 +1,44 @@
+resource "aws_iam_policy" "secretsmanager_getsecretvalue" {
+  name        = "secretsmanager_getsecretvalue"
+  description = "Permite a ação secretsmanager:GetSecretValue"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "secretsmanager:GetSecretValue",
+        Resource = "${data.aws_secretsmanager_secret.foodieFlow_secrets.arn}"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role" "eks_role" {
+  name = "eks_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy_to_role" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = aws_iam_policy.secretsmanager_getsecretvalue.arn
+}
+
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.5"
@@ -9,15 +50,17 @@ module "eks" {
   subnet_ids                               = module.vpc.public_subnets
   vpc_id                                   = module.vpc.vpc_id
 
-  eks_managed_node_groups = {
-    initial = {
-      instance_types = ["t2.micro"]
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 5
+      min_capacity     = 1
 
-      min_size     = 1
-      max_size     = 5
-      desired_size = 2
+      instance_type = "t2.micro"
+      key_name      = var.key_name
 
-      # Configurando a política de segurança para permitir tráfego na porta 8080 
+      iam_role_id = aws_iam_role.eks_role.id
+
       additional_security_group_rules = [
         {
           description       = "Allow incoming traffic on port 8080"
