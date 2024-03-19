d73d07a3aedf87274b439943902a0b3923ed5051
Guilherme-Endrigo <endrigo.guilherme@hotmail.com>
2024-03-19 11:34:14 -0300

    ajuste

--- eks.tf
+++ eks.tf
@@ -123,5 +123,23 @@ resource "aws_iam_role" "serviceaccount_role" {
       }
     ]
   })
+}
 
+#garantir provider
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster_auth.token
+  }
 }
