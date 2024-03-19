38b01d18678069fedd1183a83122abb5e47ae2c0
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-19 16:56:32 -0300

    test-18h39

--- vpc.tf
+++ vpc.tf
@@ -23,3 +23,19 @@ module "vpc" {
   map_public_ip_on_launch = true
 }
 
+ module "vpc_cni_irsa" { 
+   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks" 
+   version = "~> 4.12" 
+  
+   role_name_prefix      = "VPC-CNI-IRSA" 
+   attach_vpc_cni_policy = true 
+   vpc_cni_enable_ipv6   = true 
+  
+   oidc_providers = { 
+     main = { 
+       provider_arn               = module.eks.oidc_provider_arn 
+       namespace_service_accounts = ["kube-system:aws-node"] 
+     } 
+   } 
+   
+ } 
