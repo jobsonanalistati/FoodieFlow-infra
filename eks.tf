7427b11600b05b8263e366dcf7d9f911eb0af471
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-19 14:00:07 -0300

    test-14h00

--- eks.tf
+++ eks.tf
@@ -101,27 +101,3 @@ resource "kubernetes_namespace_v1" "foodieflownamespace" {
     module.eks
   ]
 }
-
-resource "aws_iam_role" "serviceaccount_role" {
-  name = "aws-iam-serviceaccount-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = module.eks.oidc_provider_arn
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "${module.eks.oidc_provider}:aud" : "sts.amazonaws.com",
-            "${module.eks.oidc_provider}:sub" : "system:serviceaccount:${var.app_namespace}:${var.serviceaccount_name}"
-          }
-        }
-      }
-    ]
-  })
-
-}
