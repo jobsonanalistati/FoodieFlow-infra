9c77e7a4cc858f6cf6b89d2c6dc661060bb55d7c
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-19 07:37:49 -0300

    ajuste-secret

--- provider.tf
+++ provider.tf
@@ -20,3 +20,20 @@ provider "aws" {
 }
 
 #garantir provider
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster_auth.token
+  }
+}
