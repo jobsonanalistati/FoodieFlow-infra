2eda3f21d8dc4a26d982cd809d5c4ec17358fd36
Guilherme-Endrigo <endrigo.guilherme@hotmail.com>
2024-03-19 11:20:36 -0300

    ajuste tfs

--- vpc.tf
+++ vpc.tf
@@ -1,24 +1,33 @@
 data "aws_availability_zones" "available" {
   filter {
-    name   = "zone-type"
-    values = ["availability-zone"]
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
   }
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.5.2"
+  version = "~> 5.5"
 
   name                 = "VPC-${var.projectName}-app"
-  cidr                 = "172.31.0.0/16"
-  azs                  = slice(data.aws_availability_zones.available.names, 0, 3)
-  public_subnets       = ["172.31.80.0/20", "172.31.16.0/20", "172.31.32.0/20"]
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
   enable_dns_hostnames = true
   enable_dns_support   = true
 
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  # Necessário no Kubernetes
   public_subnet_tags = {
-    "kubernetes.io/cluster/cluster-eks-${var.projectName}}-app" = "shared"
-    "kubernetes.io/role/elb"                                    = "1"
+    "kubernetes.io/role/elb" = 1
   }
-  map_public_ip_on_launch = true
-}
+
+  # Necessário no Kubernetes
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
