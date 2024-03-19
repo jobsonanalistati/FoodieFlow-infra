02dc62856595aa2a430fc8e48b61399a4fa767cb
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-19 05:44:03 -0300

    tt-2-05h43

--- provider.tf
+++ provider.tf
@@ -30,13 +30,6 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
 }
 
-provider "kubectl" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority.data)
-  load_config_file       = false
-  token                  = data.aws_eks_cluster_auth.cluster_auth.token
-}
-
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
