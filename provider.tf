409555609ea7c7cf6696c89a0bc177204d8f0ed1
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-19 05:42:18 -0300

    tt-05h42

--- provider.tf
+++ provider.tf
@@ -20,28 +20,27 @@ provider "aws" {
 }
 
 #garantir provider 
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = module.eks.cluster_name
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority.data)
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+}
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority.data)
+  load_config_file       = false
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
 }
 
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    }
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority.data)
+    token                  = data.aws_eks_cluster_auth.cluster_auth.token
   }
 }
-
